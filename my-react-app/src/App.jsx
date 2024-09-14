541bcc0aaca49cc158bccf48a1b7a690cd0b528d
Praphan <praphanhc@gmail.com>
2024-09-14 22:50:40 +0800

    forth commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,16 @@
 import Card from "./Card";
 import Button from "./Button";
 import Student from "./Student";
+import UserGreeting from "./UserGreeting";
 
 function App() {
   return (
     <div>
       <Card />
       <Button />
+      <UserGreeting isLoggedIn={true} username="Praphan" />
+      <UserGreeting isLoggedIn={false} username="Praphan" />
+      <UserGreeting isLoggedIn={false} />
       <Student name="Spongebob" age={30} isStudent={true} />
       <Student name="Patrick" age={42} isStudent={false} />
       <Student name="Mary" age={12} isStudent={true} />
