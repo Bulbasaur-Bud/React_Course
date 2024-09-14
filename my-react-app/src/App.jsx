60e55eaf52f0d92e3ccd2c44e9d731495001ca5a
Praphan <praphanhc@gmail.com>
2024-09-14 18:00:58 +0800

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./assets/Header";
+import Footer from "./assets/Footer";
+import Food from "./assets/Food";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Food />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
