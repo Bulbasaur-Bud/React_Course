8804d67f6aaec6c39c01ece953517b33db2a6e18
Praphan <praphanhc@gmail.com>
2024-09-14 22:30:07 +0800

    third commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,23 @@
 import Card from "./Card";
+import Button from "./Button";
+import Student from "./Student";
 
 function App() {
   return (
     <div>
+      <Card />
+      <Button />
+      <Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="Mary" age={12} isStudent={true} />
+      <Student />
+      <Student name="Larry" />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
       <Card />
     </div>
   );
--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,9 +1,15 @@
 function Card() {
   return (
-    <div className="Card">
-      <img></img>
-      <h2>Praphan's React Coding Lesson</h2>
-      <p>Civil to Software Engineer</p>
+    <div className="card">
+      <img
+        className="card-image"
+        src="https://media.licdn.com/dms/image/v2/D5635AQEB4F_x9vOu7Q/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1681891509265?e=1726920000&v=beta&t=p8Wqw6YXexbeOY-k9ae-wijMIZNquoFpI75BeK79TOk"
+        width="200"
+        height="200"
+        alt="profile picture"
+      ></img>
+      <h2 className="card-title">Praphan's React Coding Lesson</h2>
+      <p className="card-text">Civil to Software Engineer</p>
     </div>
   );
 }
